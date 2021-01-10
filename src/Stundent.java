bfbee3275f8c080f161d80e15dc886cb17ae0c0f
Administrator <3110235580@qq.com>
2021-01-10 15:51:51 +0800

    Stundent和teacher的封装

---
+++ src/Stundent.java
@@ -0,0 +1,38 @@
+public class Stundent {
+    private String stuId;
+    private String password;
+    private String major;
+    private String phone;
+
+    public String getStuId() {
+        return stuId;
+    }
+
+    public void setStuId(String stuId) {
+        this.stuId = stuId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ src/teacher.java
@@ -0,0 +1,29 @@
+public class teacher {
+    private String teacherId;
+    private String password;
+    private String zhicheng;
+
+    public String getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(String teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getZhicheng() {
+        return zhicheng;
+    }
+
+    public void setZhicheng(String zhicheng) {
+        this.zhicheng = zhicheng;
+    }
+}
